0c2ed6f735b8d6eff3f2faeb058ca49d2f2b4696
stonepage <1245835950@qq.com>
2018-10-30 17:41:46 +0800

    something in Set

--- set/Set.cpp
+++ set/Set.cpp
@@ -1,1 +1,40 @@
 #include "Set.hpp"
+
+#include <iostream>
+bool Set::empty() const {
+	return (root == NULL);
+}
+Set::size_type Set::size() const {
+	return siz;
+}
+void Set::clear(){
+	delTree(root);
+	siz=0;
+}
+Set::Set(){
+	root = NULL;	
+	siz=0;
+}
+Set::~Set(){
+	delTree(root);
+}
+void Set::delTree(n_ptr &root){
+	if(root == NULL) return ;
+	delTree(root -> left);
+	delTree(root -> right);
+	delete(root);
+	root = NULL;
+}
+void Set::test(){
+	using std::cout;
+	using std::endl;
+	key_type a,b;
+	a=1.0; b=2.0; 
+	cout <<b<<" "<<a<<" "<< Compare()(b,a)<<endl;
+	cout <<a<<" "<<b<<" "<< Compare()(a,b)<<endl;
+	cout<< a<<" "<<a<<" "<<Compare()(a,a)<<endl;
+}
+int main(){ // test
+	Set a; a.test();	
+	return 0;
+}
--- set/Set.hpp
+++ set/Set.hpp
@@ -1,28 +1,65 @@
 #ifndef _SET_HPP
 #define _SET_HPP
-
+#include <iostream>
 #include <functional>
 using T = double;
 using Compare = std::less<T>;
 
 class Set {
 public:
-  typedef T                 key_type;
-  typedef key_type&         reference;
-  typedef const key_type&   const_reference;
-  typedef size_t            size_type;
-  typedef Compare           key_compare;
+	typedef T                 key_type;   
+	typedef key_type&         reference;
+	typedef const key_type&   const_reference;
+	typedef size_t            size_type;
+	typedef Compare           key_compare;
 
 public:
-  bool empty() const;
-  size_type size() const;
-  void clear();
-  void insert(const key_type& key);
-  size_type erase(const key_type& key);
-  size_type count(const key_type& key);
+	bool empty() const;
+	size_type size() const;
+	void clear();
+	void insert(const key_type& key);
+	size_type erase(const key_type& key);
+	size_type count(const key_type& key);
+	Set();
+	~Set();
+	void test();
 
-private:
-  // TODO
+private: 
+	typedef bool color_type;
+	static const color_type red = true; // why static 
+	static const color_type black = false;
+	struct node;
+	typedef node* n_ptr;
+	
+	size_type siz;
+	n_ptr root; 
+	
+	void delTree(n_ptr &root);
+	struct node{
+		n_ptr parent, left, right;
+		color_type color; 
+		key_type key;
+		int cnt;
+		node(){
+			parent = left = right = NULL;
+			color = black;
+			key = (key_type)-1ll;
+			cnt = 0;
+		}
+		void debug(){
+			using std::cout;
+			using std::endl;
+			cout<<"node "<<key<<endl;
+			cout<<"parent "<<parent -> key<<endl;
+			cout<<"left   "<<left -> key<<endl;
+			cout<<"right  "<<right -> key<<endl;
+			if(color) cout<<"red"<<endl;
+			else cout<<"black"<<endl;
+			cout<<cnt<<endl;
+			cout<<"==========================="<<endl;
+		}
+	};
+	n_ptr newNode();
 }; // TODO
 
 #endif
